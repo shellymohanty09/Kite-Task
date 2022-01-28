89674deefd9123eecea12ff5bfb0f54ee4fec7c1
Shelly Mohanty <shellymohanty09@gmail.com>
2022-01-28 19:42:52 +0530

    Commit2

--- app.js
+++ app.js
@@ -105,6 +105,6 @@ app.get('/allAnime', function(req, res){
 
 
 
-app.listen(3000 || process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running");
 })
